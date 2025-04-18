acea048a92f0aa13809821c0cf44c59165868748
Lin Lin Aung <linlinaung@Lins-Mac-mini.local>
2025-04-19 02:24:51 +0630

    Fix

--- src/App.jsx
+++ src/App.jsx
@@ -76,7 +76,7 @@ const App = () => {
 
     const verifyOriginalPassword = () => {
         let message = '';
-        if (originPassword === generatedPassword) {
+        if (originPassword === allPasswords) {
             message += 'Original password is correct!<br />';
         } else {
             message += 'Original password is incorrect.<br />';
